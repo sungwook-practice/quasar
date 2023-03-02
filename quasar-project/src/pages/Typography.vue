9a45410b53f975930711fb5fa7243a8288983cc3
choisungwook <kgg1959@naver.com>
2023-03-02 21:46:14 +0900

    #2 텍스트 예제 추가

--- quasar-project/src/pages/Typography.vue
+++ quasar-project/src/pages/Typography.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
-    Text
+    <q-page class="q-pa-xl">
+      <section>
+        <p class="text-h1">Headline 1</p>
+        <p class="text-h2">Headline 2</p>
+      </section>
+    </q-page>
   </div>
 </template>
 
